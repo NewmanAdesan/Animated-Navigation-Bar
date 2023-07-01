1ec3a00ad39cdf400d6c37d169b481098c017eb9
NewmanAdesan <83893239+NewmanAdesan@users.noreply.github.com>
2023-07-01 19:40:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 
 let nav = document.getElementById("nav");
 let icon = document.getElementById("icon");
+let projectInfo = document.getElementById("project-info")
 
+
+/* Navigation Button interaction */
 icon.addEventListener("click", () => {
     nav.classList.contains("close") ? nav.classList.remove("close") : nav.classList.add("close")
     nav.querySelector("i").style.display = "none";
 })
 
 
-
+/* Project Info Close Button */
+document.getElementById("project-close").addEventListener("click", () => {
+    projectInfo.style.display = "none";
+    nav.querySelector("i").classList.add("click-me");
+})
